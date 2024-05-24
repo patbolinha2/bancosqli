db9075d4aca561b4a6610f9842719d7a6aa08113
patbolinha2 <vitorgithub887@gmail.com>
2024-05-24 16:50:45 -0300

    amen
    Jusus

---
+++ editcad.php
@@ -0,0 +1,46 @@
+<?php
+include ("conexao.php");
+$id=$_POST['id'];
+$nome=$_POST['nome'];
+$email=$_POST['email'];
+$senha= password_hash ($_POST['senha'],PASSWORD_DEFAULT);
+
+$consulta = "UPDATE usuario set nome ='$nome', email = '$email', senha = '$senha'
+WHERE id = '$id'";
+
+    if($conexao=mysqli_query($conexao, $consulta)){
+        echo"alteraçao realizada com sucesso";
+    }
+    else{
+        echo"erro ao cadastra-se".mysqli_connect_error($conexao);
+    }
+?>
+
+
+
+
+
+
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+              
+                <div class="jumbotron">
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button" style="color:black">𝓿𝓸𝓵𝓽𝓪𝓻</a>
+  </p>
+</div>
--- pesquisa.php
+++ pesquisa.php
@@ -32,9 +32,9 @@ $dados = mysqli_query($conexao, $sql);
 <table class="table table-hover">
   <thead>
     <tr>
-      <th scope="col">Nome</th>
-      <th scope="col">Email</th>
-      <th scope="col">Açoes</th>
+      <th scope="col"style="color:black;">Nome</th>
+      <th scope="col" style="color:black;">Email</th>
+      <th scope="col" style="color:black;">Açoes</th>
 
       
     </tr>
@@ -51,8 +51,8 @@ $dados = mysqli_query($conexao, $sql);
      <td>$nome</td>
      <td>$email</td>
      <td width=150px>
-     <a href='editarcard.php? id=$id' class='btn btn-success btn-sm'>editar</a>
-       <a href='' class='btn btn-danger btn-sm'>excluir</a>
+     <a href='editar.php? id=$id' class='btn btn-success btn-sm'>editar</a>
+       <a href='' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#confirma'>excluir</a>
        </td> 
      
      </tr>";
@@ -63,14 +63,35 @@ $dados = mysqli_query($conexao, $sql);
   </tbody>
   
 </table>
-            </div>
-        </div>
-    </div>
-
-
-
-
+                                      </div>
+                                      </div>
+                                      </div>
+                                      <div class="modal fade" id="confirma" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                      <div class="modal-dialog" role="document">
+                                      <div class="modal-content">
+                                      <div class="modal-header">
+                                      <h5 class="modal-title" id="exampleModalLabel">Confirmação de exclusão</h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                      </button>
+                                      </div>
+                                      <div class="modal-body">
+                                          ...
+                                      </div>
+                                      <div class="modal-footer">
+                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Não</button>
+                                      <button type="button" class="btn btn-primary">Sim</button>
+                                      </div>
+                                      </div>
+                                      </div>
+                                      </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
